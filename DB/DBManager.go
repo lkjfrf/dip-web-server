a5139ceb61a7db9e3c6cd6b292456b71759a2bf3
lkjfrf <34626154+lkjfrf@users.noreply.github.com>
2022-12-12 21:18:52 +0900

    1차 정리

--- DB/DBManager.go
+++ DB/DBManager.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/DW-inc/LauncherFileServer/setting"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -27,8 +28,7 @@ func GetDBManager() *DBManager {
 }
 
 func (db *DBManager) Init() {
-	dsn := "root:Tomatosoup22!@tcp(127.0.0.1:3306)/dip?charset=utf8mb4&parseTime=True&loc=Local"
-	//dsn := "DIPADM:P!ssw0rd@tcp(10.5.147.148:3306)/dipdb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := setting.GetStManager().DB
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
--- setting/SettingManager.go
+++ setting/SettingManager.go
@@ -8,6 +8,7 @@ type SettingHandler struct {
 	ServerType int // 0: 나, 1: 원효로1번서버, 2: 원효로2번서버
 	Port       string
 	LogPath    string
+	DB         string
 }
 
 var St_Ins *SettingHandler
@@ -21,20 +22,20 @@ func GetStManager() *SettingHandler {
 }
 
 func (st *SettingHandler) Init() {
-	st.ServerType = 0 // 0: 나, 1: 원효로1번서버, 2: 원효로2번서버, 3: 민석님
+	st.ServerType = 0 // 0: 나, 1: 원효로1번서버, 2: 원효로2번서버
 
 	switch st.ServerType {
 	case 0:
 		st.Port = ":3000"
 		st.LogPath = "/data/DIPServerLog/WebServer/"
+		st.DB = "root:Tomatosoup22!@tcp(127.0.0.1:3306)/dip?charset=utf8mb4&parseTime=True&loc=Local"
 	case 1:
 		st.Port = ":3000"
 		st.LogPath = "/data/DIPServerLog/WebServer1/"
+		st.DB = "DIPADM:P!ssw0rd@tcp(10.5.147.148:3306)/dipdb?charset=utf8mb4&parseTime=True&loc=Local"
 	case 2:
 		st.Port = ":3000"
 		st.LogPath = "/data/DIPServerLog/WebServer2/"
-	case 3:
-		st.Port = ":3000"
-		st.LogPath = "/data/DIPServerLog/WebServer/"
+		st.DB = "DIPADM:P!ssw0rd@tcp(10.5.147.148:3306)/dipdb?charset=utf8mb4&parseTime=True&loc=Local"
 	}
 }
